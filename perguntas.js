091b2dd0f784167355cb1f720d35ece916d00ac9
anaci159 <cividini.ana@escola.pr.gov.br>
2025-06-13 10:48:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? Quanto mais rugas têm mais novo é.',
+    'O pneu.'
+)
+
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? Ela vai, ela vem, e em um ponto se detém.?',
+    'A porta'
+)
+
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? O burro me leva com ele, me colocam em um baú, você não tem, mas eu tenho.',
+    'A letra “U”'
+)
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? Bebo água com os pés e respiro ao contrário. O que eu sou?',
+    'A árvore'
+)
+criaCartao(
+    'O que é, o que é?',
+    ' O que é, o que é? Na cidade é uma profissão, na estrada é um perigo e na mata é um inseto?',
+    'O barbeiro'
+)
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é que quanto maior fica, menos você vê?',
+    'A escuridão'
+)
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? Uso roupas brancas, tenho cabelos brancos e até o melhor cozinheiro chora por minha causa.',
+    'A cebola'
+)
+criaCartao(
+    'O que é, o que é?',
+    'O que é, o que é? Desde o momento em que nasci, corro de dia, corro de noite, corro sem parar, até morrer no mar.',
+    'O rio'
+)
